7a3a3577561735d8b6fbfa4ecd79ffbceae337bb
bjrajafju <bjrajafju@gmail.com>
2026-04-28 20:43:19 +0100

    agora sim, todas as paginas incluindo o header e footer

--- includes/footer.php
+++ includes/footer.php
@@ -8,10 +8,9 @@ include_once 'config.php';
         <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
             <a href="" class="text-decoration-none">
                 <h1 class="mb-4 display-5 font-weight-semi-bold"><span
-                        class="text-primary font-weight-bold border border-white px-3 mr-1">E</span>Shopper</h1>
+                        class="text-primary font-weight-bold border border-white px-3 mr-1">D</span><?= t('footer.brand.name') ?></h1>
             </a>
-            <p>Dolore erat dolor sit lorem vero amet. Sed sit lorem magna, ipsum no sit erat lorem et magna ipsum
-                dolore amet erat.</p>
+            <p><?= t('footer.description') ?></p>
             <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street, New York, USA</p>
             <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
             <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
@@ -19,51 +18,36 @@ include_once 'config.php';
         <div class="col-lg-8 col-md-12">
             <div class="row">
                 <div class="col-md-4 mb-5">
-                    <h5 class="font-weight-bold text-dark mb-4">Quick Links</h5>
+                    <h5 class="font-weight-bold text-dark mb-4"><?= t('footer.quick_links.title') ?></h5>
                     <div class="d-flex flex-column justify-content-start">
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/index.php"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/shop.php"><i class="fa fa-angle-right mr-2"></i>Our
-                            Shop</a>
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/detail.php"><i class="fa fa-angle-right mr-2"></i>Shop
-                            Detail</a>
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/cart.php"><i class="fa fa-angle-right mr-2"></i>Shopping
-                            Cart</a>
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/checkout.php"><i
-                                class="fa fa-angle-right mr-2"></i>Checkout</a>
-                        <a class="text-dark" href="<?= $SETTINGS['url_site'] ?>/contact.php"><i class="fa fa-angle-right mr-2"></i>Contact
-                            Us</a>
+                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/index.php"><i class="fa fa-angle-right mr-2"></i><?= t('footer.quick_links.home') ?></a>
+                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/shop.php"><i class="fa fa-angle-right mr-2"></i><?= t('footer.quick_links.shop') ?></a>
+                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/cart.php"><i class="fa fa-angle-right mr-2"></i><?= t('footer.quick_links.cart') ?></a>
+                        <a class="text-dark" href="<?= $SETTINGS['url_site'] ?>/contact.php"><i class="fa fa-angle-right mr-2"></i><?= t('footer.quick_links.contact') ?></a>
                     </div>
                 </div>
                 <div class="col-md-4 mb-5">
-                    <h5 class="font-weight-bold text-dark mb-4">Quick Links</h5>
+                    <h5 class="font-weight-bold text-dark mb-4"><?= t('footer.quick_links.title') ?></h5>
                     <div class="d-flex flex-column justify-content-start">
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/index.php"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/shop.php"><i class="fa fa-angle-right mr-2"></i>Our
-                            Shop</a>
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/detail.php"><i class="fa fa-angle-right mr-2"></i>Shop
-                            Detail</a>
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/cart.php"><i class="fa fa-angle-right mr-2"></i>Shopping
-                            Cart</a>
-                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/checkout.php"><i
-                                class="fa fa-angle-right mr-2"></i>Checkout</a>
-                        <a class="text-dark" href="<?= $SETTINGS['url_site'] ?>/contact.php"><i class="fa fa-angle-right mr-2"></i>Contact
-                            Us</a>
+                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/index.php"><i class="fa fa-angle-right mr-2"></i><?= t('footer.quick_links.home') ?></a>
+                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/shop.php"><i class="fa fa-angle-right mr-2"></i><?= t('footer.quick_links.shop') ?></a>
+                        <a class="text-dark mb-2" href="<?= $SETTINGS['url_site'] ?>/cart.php"><i class="fa fa-angle-right mr-2"></i><?= t('footer.quick_links.cart') ?></a>
+                        <a class="text-dark" href="<?= $SETTINGS['url_site'] ?>/contact.php"><i class="fa fa-angle-right mr-2"></i><?= t('footer.quick_links.contact') ?></a>
                     </div>
                 </div>
                 <div class="col-md-4 mb-5">
-                    <h5 class="font-weight-bold text-dark mb-4">Newsletter</h5>
+                    <h5 class="font-weight-bold text-dark mb-4"><?= t('footer.newsletter.title') ?></h5>
                     <form action="">
                         <div class="form-group">
-                            <input type="text" class="form-control border-0 py-4" placeholder="Your Name"
+                            <input type="text" class="form-control border-0 py-4" placeholder="<?= t('footer.newsletter.name_placeholder') ?>"
                                 required="required" />
                         </div>
                         <div class="form-group">
-                            <input type="email" class="form-control border-0 py-4" placeholder="Your Email"
+                            <input type="email" class="form-control border-0 py-4" placeholder="<?= t('footer.newsletter.email_placeholder') ?>"
                                 required="required" />
                         </div>
                         <div>
-                            <button class="btn btn-primary btn-block border-0 py-3" type="submit">Subscribe
-                                Now</button>
+                            <button class="btn btn-primary btn-block border-0 py-3" type="submit"><?= t('footer.newsletter.button') ?></button>
                         </div>
                     </form>
                 </div>
@@ -73,15 +57,14 @@ include_once 'config.php';
     <div class="row border-top border-light mx-xl-5 py-4">
         <div class="col-md-6 px-xl-0">
             <p class="mb-md-0 text-center text-md-left text-dark">
-                &copy; <a class="text-dark font-weight-semi-bold" href="#">Your Site Name</a>. All Rights Reserved.
-                Designed
-                by
+                &copy; <a class="text-dark font-weight-semi-bold" href="#"><?= t('footer.copyright.site_name') ?></a>. <?= t('footer.copyright.rights') ?>
+                <?= t('footer.copyright.designed_by') ?>
                 <a class="text-dark font-weight-semi-bold" href="https://htmlcodex.com">HTML Codex</a><br>
-                Distributed By <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                <?= t('footer.copyright.distributed_by') ?> <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
             </p>
         </div>
         <div class="col-md-6 px-xl-0 text-center text-md-right">
-            <img class="img-fluid" src="<?= $SETTINGS['url_site'] ?>/img/payments.png" alt="">
+            <img class="img-fluid" src="<?= $SETTINGS['url_site'] ?>/<?= t('footer.payments_image') ?>" alt="">
         </div>
     </div>
 </div>

a5b87f4bca1bd608587e1aca10a3159493729d2b
bjrajafju <bjrajafju@gmail.com>
2026-05-07 11:00:37 +0100

    sdf

--- sql/extension.sql
+++
@@ -1,5 +0,0 @@
--- Add is_admin column to users table
-ALTER TABLE `users` ADD `is_admin` TINYINT(1) DEFAULT 0;
-
--- Promote Super User to Admin
-UPDATE `users` SET `is_admin` = 1 WHERE email = 'bjrajafju@gmail.com';
